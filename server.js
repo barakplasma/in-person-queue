86026b02045535b3250894ba03d0735e4ef772fa
Michael Salaverry <michael_salaverry@intuit.com>
2021-01-20 19:11:56 +0200

    refactor: remove https (make external concern)

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 require('dotenv').config()
-const keyPath = process.env.KEY || "localhost-key.pem";
-const certPath = process.env.CERT || "localhost-cert.pem";
 const PORT = process.env.PORT || "3000";
 
 const { static } = require('express');
 const app = require('express')();
 
-const fs = require('fs');
-const server = require('https').createServer({
-  key: fs.readFileSync(keyPath),
-  cert: fs.readFileSync(certPath)
-}, app);
+const server = require('http').createServer(app);
 require('./socket/connection').connection(server);
 
 app.use(static('client'))
