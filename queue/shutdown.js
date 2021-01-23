95ee6767984e6436977dcf91a79756752c6535a2
Michael Salaverry <barakplasma@gmail.com>
2021-01-23 13:02:22 +0200

    feat: add graceful shutdown

---
+++ queue/shutdown.js
@@ -0,0 +1,53 @@
+// see https://github.com/godaddy/terminus/blob/master/example/redis/index.js for inspiration
+
+const _redis = require('./queue')._redis;
+const http = require('http');
+
+const PORT = process.env.PORT || "3000";
+
+/**
+ * Make a server-side request; similar to Got
+ * @param {URL} url 
+ * @returns {number} statusCode
+ */
+const getStatusOfUrl = url => new Promise((resolve, rejects) => {
+  http.get(url, (res) => {
+    const { statusCode } = res;
+    if (statusCode === 200) {
+      resolve({ statusCode, url })
+    } else {
+      rejects({ statusCode, url })
+    }
+  })
+})
+
+/**
+ * @type {import('@godaddy/terminus').TerminusOptions}
+ */
+const terminusOptions = {
+  onSignal: function () {
+    console.log('server is starting cleanup');
+    return Promise.all([
+      // your clean logic, like closing database connections
+      _redis
+        .quit()
+        .then(() => console.log('redis disconnected'))
+        .catch(err => console.error('error during disconnection', err.stack))
+    ]);
+  },
+  onShutdown: function () {
+    console.log('cleanup finished, server is shutting down');
+  },
+  healthChecks: {
+    '/healthcheck': function () {
+      return Promise.all([
+        getStatusOfUrl(new URL(`http://localhost:${PORT}/`)),
+        getStatusOfUrl(new URL(`http://localhost:${PORT}/admin.html`)),
+        getStatusOfUrl(new URL(`http://localhost:${PORT}/queue.html`)),
+        _redis.status === 'ready' ? Promise.resolve(_redis.status) : Promise.reject(new Error(_redis.status))
+      ]);
+    }
+  }
+}
+
+module.exports.terminusOptions = terminusOptions;
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || "3000";
 const { static } = require('express');
 const app = require('express')();
 
+const { createTerminus } = require('@godaddy/terminus');
+const terminusOptions = require('./queue/shutdown').terminusOptions;
+
 const server = require('http').createServer(app);
 require('./socket/connection').connection(server);
 
@@ -14,6 +17,8 @@ app.get('/queue/*', (_, res) => {
 })
 
 app.route('/').all(static('client'));
+
+createTerminus(server, terminusOptions);
 
 server.listen(PORT, () => {
   console.log('listening on *:'+PORT);
