cc307ac86c61a558ef5c0420c6910c28e1ba271c
Michael Salaverry <michael_salaverry@intuit.com>
2021-08-28 14:38:22 +0300

    feat: add share link to admin page

--- client/admin.js
+++ client/admin.js
@@ -33,9 +33,50 @@ function currentUserDone() {
   refreshAdminPage();
 }
 
+function generateShareURL() {
+  let params = new URLSearchParams(location.search);
+  let shareLinkUrl = new URL(window.location.origin + '/queue.html');
+  shareLinkUrl.searchParams.set('location', params.get('location'));
+  return shareLinkUrl;
+}
+
+function generateShareLink() {
+  let shareLinkUrl = generateShareURL();
+  updateHTML('#shareLink', `link to join queue as user: <a href="${shareLinkUrl.href}">link</a><a id="shareButton"></a>`);
+  generateShareButton();
+}
+
+function generateShareButton() {
+  if (navigator.share) {
+    let shareLinkUrl = generateShareURL();
+
+    const shareData = {
+      title: 'Join Queue',
+      text: 'Join Queue at ' + atob(shareLinkUrl.searchParams.get('location')),
+      url: shareLinkUrl.href,
+    }
+
+    let btn = document.querySelector('#shareButton')
+    updateHTML('#shareButton', '<img src="share.png">');
+
+    if (btn) {
+      async function share() {
+        try {
+          await navigator.share(shareData)
+        } catch(err) {
+          console.warn(err);
+        }
+      }
+      btn.addEventListener('click', share);
+      btn.addEventListener('touchstart', share);
+    }
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   joinQueue('admin');
   adminSocket.emit('join-queue', getQueueFromAddressOrCache());
+  generateShareLink();
   refreshAdminPage();
 });
 
